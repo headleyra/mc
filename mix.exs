856057192ff8b27691b0b3e1742bae3320e8bed2
RH <rh@example.com>
2020-11-11 01:50:24 +0000

    mix.exs: removed unneeded commented code

--- mix.exs
+++ mix.exs
@@ -27,7 +27,6 @@ defmodule Mc.MixProject do
       {:jason, "~> 1.1.2"},
       {:elixir_uuid, "~> 1.2"},
       {:tesla, "~> 1.3.3"}
-      # {:mongodb, "~> 0.5.1"},
 
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
