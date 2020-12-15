c545ab67fe45394f1805abe9f43b4fa6b33b3c91
RH <rh@example.com>
2020-12-15 19:03:56 +0000

    Refactored htab modifier

--- lib/mc/modifier/htab.ex
+++ lib/mc/modifier/htab.ex
@@ -5,37 +5,43 @@ defmodule Mc.Modifier.Htab do
   def modify("", _args), do: {:ok, ""}
 
   def modify(buffer, args) do
+    case parse(args) do
+      {:ok, row_css, col_css} ->
+        {:ok, html} = Floki.parse_fragment(buffer)
+
+        result =
+          Floki.find(html, row_css)
+          |> Enum.map(fn row_tags -> Floki.find(row_tags, col_css) end)
+          |> Enum.map(fn col_tags -> Enum.map(col_tags, fn tag -> Floki.text(tag) end) end)
+          |> Enum.map(fn list -> Enum.map(list, fn e -> String.trim(e) end) end)
+          |> Enum.map(fn content -> Enum.join(content, "\t") end)
+          |> Enum.reject(&(&1 == ""))
+          |> Enum.join("\n")
+          |> Kernel.<>("\n")
+
+        {:ok, result}
+
+      _error ->
+        usage(:modify, @argspec)
+    end
+  end
+
+  defp parse(args) do
     case String.split(args) do
       [row, col] ->
         row_css = Mc.Util.InlineString.uri_decode(row)
         col_css = Mc.Util.InlineString.uri_decode(col)
-        {:ok, html} = Floki.parse_fragment(buffer)
 
-        ok_row_col =
-          [row_css, col_css]
-          |> Enum.all?(fn e -> match?({:ok, _}, e) end)
+        case [row_css, col_css] do
+          [{:ok, ok_row_css}, {:ok, ok_col_css}] ->
+            {:ok, ok_row_css, ok_col_css}
 
-        if ok_row_col do
-          {:ok, ok_row_css} = row_css
-          {:ok, ok_col_css} = col_css
-
-          result =
-            Floki.find(html, ok_row_css)
-            |> Enum.map(fn row_tags -> Floki.find(row_tags, ok_col_css) end)
-            |> Enum.map(fn col_tags -> Enum.map(col_tags, fn tag -> Floki.text(tag) end) end)
-            |> Enum.map(fn list -> Enum.map(list, fn e -> String.trim(e) end) end)
-            |> Enum.map(fn content -> Enum.join(content, "\t") end)
-            |> Enum.reject(&(&1 == ""))
-            |> Enum.join("\n")
-            |> Kernel.<>("\n")
-
-          {:ok, result}
-        else
-          usage(:modify, @argspec)
+          _error ->
+            :error
         end
 
-      _bad_args ->
-        usage(:modify, @argspec)
+      _error ->
+        :error
     end
   end
 end
