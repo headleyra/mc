e11ac2af71be9a8f133fef194c3ed7e83a8d2dc0
RH <rh@example.com>
2020-12-15 02:11:16 +0000

    Refactored replace modifier

--- lib/mc/modifier/replace.ex
+++ lib/mc/modifier/replace.ex
@@ -3,27 +3,35 @@ defmodule Mc.Modifier.Replace do
   @argspec "<search regex> <replace string>"
 
   def modify(buffer, args) do
-    case String.split(args, " ", parts: 2) do
-      [search, replace] ->
-        case Regex.compile(search, "sm") do
-          {:ok, _} ->
-            {:ok, search_regex} = Regex.compile(search, "sm")
-
-            case Mc.Util.InlineString.uri_decode(replace) do
-              {:ok, uri_decoded_replace} ->
-                result = String.replace(buffer, search_regex, uri_decoded_replace)
-                {:ok, result}
-
-              _error ->
-                usage(:modify, @argspec)
-            end
-
-          _error ->
-            usage(:modify, @argspec)
-        end
+    case parse(args) do
+      {:ok, search_regex, uri_decoded_replace} ->
+        result = String.replace(buffer, search_regex, uri_decoded_replace)
+        {:ok, result}
 
       _error ->
         usage(:modify, @argspec)
+    end
+  end
+
+  defp parse(args) do
+    case String.split(args, " ", parts: 2) do
+      [search, replace] ->
+        case Regex.compile(search, "sm") do
+          {:ok, search_regex} ->
+            case Mc.Util.InlineString.uri_decode(replace) do
+              {:ok, uri_decoded_replace} ->
+                {:ok, search_regex, uri_decoded_replace}
+
+              _error ->
+                :error
+            end
+
+          _error ->
+            :error
+        end
+
+      _error ->
+        :error
     end
   end
 end
