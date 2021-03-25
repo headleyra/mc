a86b80d9b9c560ac6c2acc65e77595e232935ec0
RH <rh@example.com>
2021-03-25 12:19:46 +0000

    - Loosened deps version requirements - Ran `mix deps.update --all`

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Mc.MixProject do
   def project do
     [
       app: :mc,
-      version: "0.32.1",
+      version: "0.33.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps()
@@ -22,11 +22,11 @@ defmodule Mc.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:floki, "~> 0.26.0"},
-      {:httpoison, "~> 1.6.2"},
-      {:jason, "~> 1.2.2"},
+      {:floki, "~> 0.26"},
+      {:httpoison, "~> 1.6"},
+      {:jason, "~> 1.2"},
       {:elixir_uuid, "~> 1.2"},
-      {:tesla, "~> 1.3.3"}
+      {:tesla, "~> 1.3"}
 
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
