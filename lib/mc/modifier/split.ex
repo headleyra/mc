9439b1a169a415caedfa75a0794d2bffc188780a
RH <rh@example.com>
2021-10-11 04:07:42 +0100

    Changed the 'split' modifier to split on whitespace by default

--- lib/mc/modifier/split.ex
+++ lib/mc/modifier/split.ex
@@ -2,11 +2,12 @@ defmodule Mc.Modifier.Split do
   use Mc.Railway, [:modify]
   @argspec "<regex>"
 
+  def modify(buffer, ""), do: {:ok, String.split(buffer) |> Enum.join("\n")}
+
   def modify(buffer, args) do
     case Regex.compile(args) do
       {:ok, regex} ->
-        result = String.split(buffer, regex) |> Enum.join("\n")
-        {:ok, result}
+        {:ok, String.split(buffer, regex) |> Enum.join("\n")}
 
       {:error, _} ->
         usage(:modify, @argspec)
--- test/mc/modifier/split_test.exs
+++ test/mc/modifier/split_test.exs
@@ -3,7 +3,13 @@ defmodule Mc.Modifier.SplitTest do
   alias Mc.Modifier.Split
 
   describe "Mc.Modifier.Split.modify/2" do
-    test "splits the buffer by `args`" do
+    test "splits the buffer on whitespace by default" do
+      assert Split.modify("un deux trois", "") == {:ok, "un\ndeux\ntrois"}
+      assert Split.modify("foo    bar      biz", "") == {:ok, "foo\nbar\nbiz"}
+      assert Split.modify("mix \n it  \t up\n\n", "") == {:ok, "mix\nit\nup"}
+    end
+
+    test "splits the buffer on `args`" do
       assert Split.modify("one-two-three", "-") == {:ok, "one\ntwo\nthree"}
       assert Split.modify("= double == equals =", "=") == {:ok, "\n double \n\n equals \n"}
     end
@@ -18,7 +24,7 @@ defmodule Mc.Modifier.SplitTest do
     end
 
     test "works with ok tuples" do
-      assert Split.modify({:ok, "best.of.3"}, "\\.") == {:ok, "best\nof\n3"}
+      assert Split.modify({:ok, "best-of-3"}, "-") == {:ok, "best\nof\n3"}
     end
 
     test "allows error tuples to pass-through" do
