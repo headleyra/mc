3dc37b4d28cb4ff07ca5cd39b0cd9f8e396e4fa0
RH <rh@example.com>
2022-04-27 13:21:22 +0100

    Removed the 'ifg' and 'ifl' modifiers and replaced them with a 'super simple' 'if' modifier

--- lib/mc/modifier/if.ex
+++ lib/mc/modifier/if.ex
@@ -1,33 +1,30 @@
 defmodule Mc.Modifier.If do
-  use Mc.Railway, [:modifye, :modifyl, :modifyg]
+  use Mc.Railway, [:modify, :modifye]
 
-  def modifye(buffer, args) do
-    compare(buffer, args, fn a, b -> a == b end, :modifye)
-  end
-
-  def modifyl(buffer, args) do
-    compare(buffer, args, fn a, b -> a < b end, :modifyl)
-  end
-
-  def modifyg(buffer, args) do
-    compare(buffer, args, fn a, b -> a > b end, :modifyg)
-  end
-
-  defp compare(buffer, args, compare_func, from_func) do
+  def modify(buffer, args) do
     case String.split(args) do
-      [compare_key, true_key, false_key] ->
-        {:ok, compare_value} = Mc.modify("", "get #{compare_key}")
-
-        if compare_func.(buffer, compare_value) do
-          {:ok, true_script} = Mc.modify("", "get #{true_key}")
-          Mc.modify(buffer, true_script)
-        else
-          {:ok, false_script} = Mc.modify("", "get #{false_key}")
-          Mc.modify(buffer, false_script)
-        end
+      [script_key] ->
+        if String.match?(buffer, ~r/^\s*$/), do: get(buffer, script_key), else: {:ok, buffer}
 
       _bad_args ->
-        usage(from_func, "<compare key> <true key> <false key>")
+        usage(:modify, "<script key>")
     end
+  end
+
+  def modifye(buffer, args) do
+    with \
+      [compare_key, true_key, false_key] <- String.split(args),
+      {:ok, compare_value} = Mc.modify("", "get #{compare_key}")
+    do
+      if buffer == compare_value, do: get(buffer, true_key), else: get(buffer, false_key)
+    else
+      _bad_args ->
+        usage(:modifye, "<compare key> <true key> <false key>")
+    end
+  end
+
+  defp get(buffer, key) do
+    {:ok, script} = Mc.modify("", "get #{key}")
+    Mc.modify(buffer, script)
   end
 end
--- test/mc/modifier/if_test.exs
+++ test/mc/modifier/if_test.exs
@@ -4,24 +4,50 @@ defmodule Mc.Modifier.IfTest do
 
   setup do
     start_supervised({Mc.Modifier.Kv, map: %{
-      "a_key" => "aaa",
-      "z_key" => "zzz",
+      "key.a" => "aaa",
+      "key.z" => "zzz",
+      "key.1" => "b forda money",
+      "key.2" => "b 4the show",
+      "apd" => "append this",
       "small" => "lcase",
       "big" => "ucase",
-      "empty.key" => ""
+      "empty" => ""
     }})
     :ok
   end
 
+  describe "Mc.Modifier.If.modify/2" do
+    test "runs 'value of key' if `buffer` is 'blank', else returns `buffer`" do
+      assert If.modify("", "key.1") == {:ok, "forda money"}
+      assert If.modify("not blank", "na") == {:ok, "not blank"}
+      assert If.modify("  ", "key.2") == {:ok, "4the show"}
+      assert If.modify("foo\t\nbar", "na") == {:ok, "foo\t\nbar"}
+    end
+
+    test "errors without exactly 1 key" do
+      assert If.modify("n/a", "") == {:error, "usage: Mc.Modifier.If#modify <script key>"}
+      assert If.modify("", "a b") == {:error, "usage: Mc.Modifier.If#modify <script key>"}
+    end
+
+    test "works with ok tuples" do
+      assert If.modify({:ok, ""}, "key.1") == {:ok, "forda money"}
+      assert If.modify({:ok, "dosh"}, "na") == {:ok, "dosh"}
+    end
+
+    test "allows error tuples to pass-through" do
+      assert If.modify({:error, "reason"}, "na") == {:error, "reason"}
+    end
+  end
+
   describe "Mc.Modifier.If.modifye/2" do
-    test "evaluates `buffer` == 'value-of-arg1-key'? and runs the script in 'arg2' if true, else 'arg3'" do
-      assert If.modifye("aaa", "a_key big small") == {:ok, "AAA"}
-      assert If.modifye("NOT EQUAL", "z_key big small") == {:ok, "not equal"}
-      assert If.modifye("aaa", "a_key empty.key oops") == {:ok, "aaa"}
+    test "runs 'value of key2' if `buffer` == 'value of key1' else runs 'value of key3'" do
+      assert If.modifye("aaa", "key.a big small") == {:ok, "AAA"}
+      assert If.modifye("NOT EQUAL", "key.z big small") == {:ok, "not equal"}
+      assert If.modifye("aaa", "key.a empty oops") == {:ok, "aaa"}
       assert If.modifye("aaa", "noexist small big") == {:ok, "AAA"}
     end
 
-    test "errors when exactly 3 keys aren't provided" do
+    test "errors without exactly 3 keys" do
       assert If.modifye("fiscal rules", "") ==
         {:error, "usage: Mc.Modifier.If#modifye <compare key> <true key> <false key>"}
 
@@ -33,66 +59,11 @@ defmodule Mc.Modifier.IfTest do
     end
 
     test "works with ok tuples" do
-      assert If.modifye({:ok, "aaa"}, "a_key big any") == {:ok, "AAA"}
+      assert If.modifye({:ok, "aaa"}, "key.a big any") == {:ok, "AAA"}
     end
 
     test "allows error tuples to pass-through" do
       assert If.modifye({:error, "reason"}, "akey") == {:error, "reason"}
-    end
-  end
-
-  describe "Mc.Modifier.If.modifyl/2" do
-    test "evaluates `buffer` < 'value-of-arg1-key'? and runs the script in 'arg2' if true, else 'arg3'" do
-      assert If.modifyl("zzz", "a_key small big") == {:ok, "ZZZ"}
-      assert If.modifyl("GoLd", "z_key small big") == {:ok, "gold"}
-      assert If.modifyl("big up", "empty.key n/a big") == {:ok, "BIG UP"}
-      assert If.modifyl("zzz", "z_key small big") == {:ok, "ZZZ"}
-    end
-
-    test "errors when exactly 3 keys aren't provided" do
-      assert If.modifyl("fiscal rules", "") ==
-        {:error, "usage: Mc.Modifier.If#modifyl <compare key> <true key> <false key>"}
-
-      assert If.modifyl("gig economy", "one") ==
-        {:error, "usage: Mc.Modifier.If#modifyl <compare key> <true key> <false key>"}
-
-      assert If.modifyl("yaba daba do", "buy the dip dude") ==
-        {:error, "usage: Mc.Modifier.If#modifyl <compare key> <true key> <false key>"}
-    end
-
-    test "works with ok tuples" do
-      assert If.modifyl({:ok, "aaa"}, "z_key big small") == {:ok, "AAA"}
-    end
-
-    test "allows error tuples to pass-through" do
-      assert If.modifyl({:error, "reason"}, "akey") == {:error, "reason"}
-    end
-  end
-
-  describe "Mc.Modifier.If.modifyg/2" do
-    test "evaluates `buffer` > 'value-of-arg1-key'? and runs the script in 'arg2' if true, else 'arg3'" do
-      assert If.modifyg("zzz", "a_key big small") == {:ok, "ZZZ"}
-      assert If.modifyg("GoLd", "z_key small big") == {:ok, "GOLD"}
-      assert If.modifyg("big up", "empty.key big nope") == {:ok, "BIG UP"}
-    end
-
-    test "errors when exactly 3 keys aren't provided" do
-      assert If.modifyg("fiscal rules", "") ==
-        {:error, "usage: Mc.Modifier.If#modifyg <compare key> <true key> <false key>"}
-
-      assert If.modifyg("gig economy", "one") ==
-        {:error, "usage: Mc.Modifier.If#modifyg <compare key> <true key> <false key>"}
-
-      assert If.modifyg("yaba daba do", "buy the dip dude") ==
-        {:error, "usage: Mc.Modifier.If#modifyg <compare key> <true key> <false key>"}
-    end
-
-    test "works with ok tuples" do
-      assert If.modifyg({:ok, "aaa"}, "z_key small big") == {:ok, "AAA"}
-    end
-
-    test "allows error tuples to pass-through" do
-      assert If.modifyg({:error, "reason"}, "akey") == {:error, "reason"}
     end
   end
 end
