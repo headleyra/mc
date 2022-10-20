9595f951d525b3c98a8635ed488805ca0ef74775
RH <rh@example.com>
2022-10-20 07:47:07 +0100

    Super mini tweaks to the 'split' modifier tests

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Mc.MixProject do
   def project do
     [
       app: :mc,
-      version: "0.56.2",
+      version: "0.56.3",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps()
--- test/mc/modifier/split_test.exs
+++ test/mc/modifier/split_test.exs
@@ -3,20 +3,20 @@ defmodule Mc.Modifier.SplitTest do
   alias Mc.Modifier.Split
 
   describe "Mc.Modifier.Split.modify/2" do
+    test "splits the buffer on `args`" do
+      assert Split.modify("one-two-three", "-") == {:ok, "one\ntwo\nthree"}
+      assert Split.modify("= double == equals =", "=") == {:ok, "\n double \n\n equals \n"}
+    end
+
     test "splits the buffer on whitespace by default" do
       assert Split.modify("un deux trois", "") == {:ok, "un\ndeux\ntrois"}
       assert Split.modify("foo    bar      biz", "") == {:ok, "foo\nbar\nbiz"}
       assert Split.modify("mix \n it  \t up\n\n", "") == {:ok, "mix\nit\nup"}
     end
 
-    test "splits the buffer on `args`" do
-      assert Split.modify("one-two-three", "-") == {:ok, "one\ntwo\nthree"}
-      assert Split.modify("= double == equals =", "=") == {:ok, "\n double \n\n equals \n"}
-    end
-
-    test "interprets `args` as a regular expression" do
+    test "parses `args` as a regular expression" do
       assert Split.modify("one two three", "[ ]") == {:ok, "one\ntwo\nthree"}
-      assert Split.modify("= double == equals =", "( == )") == {:ok, "= double\nequals ="}
+      assert Split.modify("= double == equals =", " == ") == {:ok, "= double\nequals ="}
     end
 
     test "errors with a bad regular expression" do
