0d40cfb581ada9b5153e5fc7bbd130f4e3c26bdc
RH <rh@example.com>
2023-12-16 20:29:44 +0000

    Make the 'mappings (modifiers exist)' test less brittle

--- test/mc/mappings_test.exs
+++ test/mc/mappings_test.exs
@@ -1,18 +1,17 @@
 defmodule Mc.MappingsTest do
-  use ExUnit.Case, async: false
-  alias Mc.Adapter.KvMemory
-
-  setup do
-    start_supervised({KvMemory, map: %{}})
-    :ok
-  end
+  use ExUnit.Case, async: true
 
   describe "%Mc.Mappings{}" do
     test "defines modifiers that exist" do
       %Mc.Mappings{}
       |> Map.from_struct()
       |> Map.values()
-      |> Enum.each(fn module -> apply(module, :modify, ["", "", %Mc.Mappings{}]) end)
+      |> Enum.each(fn module -> exists?(module) end)
     end
+  end
+
+  defp exists?(module) do
+    Code.ensure_loaded(module)
+    assert function_exported?(module, :modify, 3), "#{module} needs to define a modify/3 function"
   end
 end
