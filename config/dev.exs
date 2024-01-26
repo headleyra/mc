a147b7b1d2a5805cb466b7b81da00c7fafe798ab
RH <rh@example.com>
2024-01-26 08:38:00 +0000

    Configure a bare bones dev environment

--- config/dev.exs
+++ config/dev.exs
@@ -1,6 +1,5 @@
-#import Config
+import Config
 
-# Use these or roll your own
-#config :mc, :kv_adapter, Mc.Adapter.KvMemory
+config :mc, :kv_adapter, Mc.Adapter.KvMemory
 #config :mc, :http_adapter, Mc.Adapter.Http
 #config :mc, :mail_adapter, Mc.Adapter.Mailgun
