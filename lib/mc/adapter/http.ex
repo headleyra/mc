5831ee4d6b637177a8bfed96ad676e16ee272e94
RH <rh@example.com>
2025-09-14 16:45:40 +0100

    Factor-out standard HTTP (:req) functionality (to make re-use possible)

--- lib/mc/adapter/http.ex
+++ lib/mc/adapter/http.ex
@@ -1,38 +1,19 @@
 defmodule Mc.Adapter.Http do
   @behaviour Mc.Behaviour.HttpAdapter
-  @user_agent "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.10 Safari/605.1.1"
-  @timeout 40_000
 
   @impl true
   def get(url) do
     url
-    |> base_request()
+    |> Mc.Http.request()
     |> Req.get()
-    |> parse()
+    |> Mc.Http.parse()
   end
 
   @impl true
   def post(url, params_list) do
     url
-    |> base_request()
+    |> Mc.Http.request()
     |> Req.post(form: params_list)
-    |> parse()
-  end
-
-  defp base_request(url) do
-    Req.new(url: url, decode_body: false, headers: %{timeout: @timeout, user_agent: @user_agent})
-  end
-
-  defp parse(response) do
-    case response do
-      {:ok, %Req.Response{status: 404}} ->
-        {:error, "http 404"}
-
-      {:ok, %Req.Response{body: body}} ->
-        {:ok, body}
-
-      {:error, reason} ->
-        {:error, inspect(reason)}
-    end
+    |> Mc.Http.parse()
   end
 end
---
+++ lib/mc/http.ex
@@ -0,0 +1,26 @@
+defmodule Mc.Http do
+  @timeout 40_000
+  @user_agent "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.10 Safari/605.1.1"
+
+  def request(url) do
+    url
+    |> base_request()
+  end
+
+  def parse(response) do
+    case response do
+      {:ok, %Req.Response{status: 404}} ->
+        {:error, "http 404"}
+
+      {:ok, %Req.Response{body: body}} ->
+        {:ok, body}
+
+      {:error, reason} ->
+        {:error, inspect(reason)}
+    end
+  end
+
+  defp base_request(url) do
+    Req.new(url: url, decode_body: false, headers: %{timeout: @timeout, user_agent: @user_agent})
+  end
+end
