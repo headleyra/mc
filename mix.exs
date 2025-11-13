2eff68d1fcd5d5135764e2b90d4a9a1561c7349e
RH <rh@example.com>
2025-11-13 21:21:23 +0000

    Upgrade: credo 1.7.12 => 1.7.13 req 0.5.15 => 0.5.16

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Mc.MixProject do
   def project do
     [
       app: :mc,
-      version: "0.103.2",
+      version: "0.103.3",
       elixir: "~> 1.17",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
