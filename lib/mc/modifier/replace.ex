d66a56f95f67bdb57c45a6adaf97af2ee796fc45
RH <rh@example.com>
2026-03-07 10:23:44 +0000

    Super tiny refactor of the 'replace' modifier

--- lib/mc/modifier/replace.ex
+++ lib/mc/modifier/replace.ex
@@ -2,17 +2,17 @@ defmodule Mc.Modifier.Replace do
   use Mc.Modifier
 
   def m(buffer, args, _mappings) do
-    case parse_replace(args) do
+    case parse(args) do
       {:ok, search_regex, uri_decoded_replace} ->
         result = String.replace(buffer, search_regex, uri_decoded_replace)
         {:ok, result}
 
-      _error ->
+      :error ->
         oops("bad search/replace or search regex")
     end
   end
 
-  defp parse_replace(args) do
+  defp parse(args) do
     with \
       [search, replace] <- String.split(args, ~r/\s+/, parts: 2),
       {:ok, search_regex} <- Regex.compile(search, "sm")
@@ -20,7 +20,8 @@ defmodule Mc.Modifier.Replace do
       uri_decoded_replace = URI.decode(replace)
       {:ok, search_regex, uri_decoded_replace}
     else
-      _error -> :error
+      _parse_error ->
+        :error
     end
   end
 end
